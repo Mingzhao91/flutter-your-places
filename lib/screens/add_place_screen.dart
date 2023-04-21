05f995f29f7e7831a720e8965d6e5873ad765312
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-04-21 20:10:30 +0100

    adding 'Add Place screen' and image input

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/image_input.dart';
+
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
 
@@ -10,6 +12,8 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +21,37 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
           title: Text('Add a New Place'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text('User Inputs...'),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: _titleController,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ImageInput(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             ElevatedButton.icon(
               icon: Icon(Icons.add),
               label: Text('Add Place'),
               onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                foregroundColor: Colors.black,
+              ),
             )
           ],
         ));
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/add_place_screen.dart';
+
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key key}) : super(key: key);
 
@@ -10,7 +12,9 @@ class PlacesListScreen extends StatelessWidget {
         title: Text('Your places'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
             icon: Icon(Icons.add),
           ),
         ],
